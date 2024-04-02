1125400e13bbb5e13293ebb222c278341bc3b724
M-Y-Sun <sun.m.yixuan@gmail.com>
2024-04-02 10:41:01 -0700

    changed variable names to all caps modified warning message to only show filename instead of try and print the absolute path, and shows 'WARNING' in brackets

--- pdfm.sh
+++ pdfm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-str="$@"
-out=$(echo $str | awk '{print $1;}')
-in=$(echo $str | awk '{$1= ""; print $0}' | xargs)
-if [ -f $out ]; then
-    terminate="\0"
-    while [ "$terminate" != "y" -a "$terminate" != "n" ]; do
-    printf "\033[33;1mWARNING\033[0m: output file will overwrite existing file \'$(find $(cd ..; pwd) -name $out)\', proceed?(y,n) "
-    read terminate
-    if [ "$terminate" = "n" ]; then
+STR="$@"
+OUT=$(echo $STR | awk '{print $1;}')
+IN=$(echo $STR | awk '{$1= ""; print $0}' | xargs)
+if [ -f $OUT ]; then
+    TERMINATE="\0"
+    while [ "$TERMINATE" != "y" -a "$TERMINATE" != "n" ]; do
+    printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'$OUT\', proceed?(y,n) "
+    read TERMINATE
+    if [ "$TERMINATE" = "n" ]; then
         exit 0
     fi
     done
 fi
-gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$out $in
+gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$OUT $IN
