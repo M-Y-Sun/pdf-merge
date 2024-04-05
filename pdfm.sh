e8a244f459aefd000e7eac5b6788dae26d2ee7be
M-Y-Sun <sun.m.yixuan@gmail.com>
2024-04-04 18:59:44 -0700

    added double quotes around variables to avoid confusion changed [ n -a m ] into [ n ] && [ m ] changed read into read -r

--- pdfm.sh
+++ pdfm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-STR="$@"
-OUT=$(echo $STR | awk '{print $1;}')
-IN=$(echo $STR | awk '{$1= ""; print $0}' | xargs)
-if [ -f $OUT ]; then
+STR="$*"
+OUT=$(echo "$STR" | awk '{print $1;}')
+IN=$(echo "$STR" | awk '{$1= ""; print $0}' | xargs)
+if [ -f "$OUT" ]; then
     TERMINATE="\0"
-    while [ "$TERMINATE" != "y" -a "$TERMINATE" != "n" ]; do
-    printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'$OUT\', proceed?(y,n) "
-    read TERMINATE
+    while [ "$TERMINATE" != "y" ] && [ "$TERMINATE" != "n" ]; do
+    printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'%s\', proceed?(y,n) " "$OUT"
+    read -r TERMINATE
     if [ "$TERMINATE" = "n" ]; then
         exit 0
     fi
     done
 fi
-gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$OUT $IN
+gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="$OUT" "$IN"
