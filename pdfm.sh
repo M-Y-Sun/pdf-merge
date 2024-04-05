ae3312f6f212082aaa34a0f5f17104fe34fbbfcf
M-Y-Sun <sun.m.yixuan@gmail.com>
2024-04-04 19:01:40 -0700

    changed options y and n to options Y and n

--- pdfm.sh
+++ pdfm.sh
@@ -5,8 +5,8 @@ OUT=$(echo "$STR" | awk '{print $1;}')
 IN=$(echo "$STR" | awk '{$1= ""; print $0}' | xargs)
 if [ -f "$OUT" ]; then
     TERMINATE="\0"
-    while [ "$TERMINATE" != "y" ] && [ "$TERMINATE" != "n" ]; do
-    printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'%s\', proceed?(y,n) " "$OUT"
+    while [ "$TERMINATE" != "Y" ] && [ "$TERMINATE" != "n" ]; do
+    printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'%s\', proceed?(^,n) " "$OUT"
     read -r TERMINATE
     if [ "$TERMINATE" = "n" ]; then
         exit 0
