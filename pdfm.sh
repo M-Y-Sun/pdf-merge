afb7acabd561420f220e230648562901b5b7641b
M-Y-Sun <sun.m.yixuan@gmail.com>
2024-09-21 11:40:55 -0700

    remove quiet output

--- pdfm.sh
+++ pdfm.sh
@@ -6,12 +6,12 @@ IN=$(echo "$STR" | awk '{$1= ""; print $0}' | xargs)
 if [ -f "$OUT" ]; then
     TERMINATE="\0"
     while [ "$TERMINATE" != "Y" ] && [ "$TERMINATE" != "n" ]; do
-    printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'%s\', proceed?(Y,n) " "$OUT"
-    read -r TERMINATE
-    if [ "$TERMINATE" = "n" ]; then
-        exit 0
-    fi
+        printf "[ \033[33;1mWARNING\033[0m ] output will overwrite existing file \'%s\', proceed?(Y,n) " "$OUT"
+        read -r TERMINATE
+        if [ "$TERMINATE" = "n" ]; then
+            exit 0
+        fi
     done
 fi
 
-gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$OUT $IN
+gs -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$OUT $IN
